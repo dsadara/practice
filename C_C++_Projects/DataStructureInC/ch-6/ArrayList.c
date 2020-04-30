7ffc2c836b84b89d8588418daf4a49b8741b1794
dsadara <lim971230@yonsei.ac.kr>
2020-04-30 20:43:59 +0900

    Add functions

--- C_C++_Projects/DataStructureInC/ch-6/ArrayList.c
+++ C_C++_Projects/DataStructureInC/ch-6/ArrayList.c
@@ -1,6 +1,7 @@
 // 배열로 구현한 리스트
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_LIST_SIZE 100
 
 typedef int element;    // 항목의 정의
@@ -46,6 +47,8 @@ element_get_entry(ArrayListType *L, int pos)
 void print_list(ArrayListType *L)
 {
     int i;
+    if(L->size == 0)
+        printf("List is empty");
     for(i = 0 ; i < L->size ; i++)
         printf("%d->", L->array[i]);
     printf("\n");
@@ -85,15 +88,70 @@ element delete(ArrayListType *L, int pos)
     return item;    // 삭제한 원소 반환
 }
 
+// 첫번째 위치에 원소를 삽입하는 함수 
+void insert_first(ArrayListType *L, element item)
+{
+    if (L->size >= MAX_LIST_SIZE)
+        error("리스트 오버플로우");
+    for (int i = (L->size - 1) ; i >= 0 ; i-- ) // 첫번째 원소부터 마지막 원소까지 오른쪽으로 한 칸씩 이동
+    {   
+        L->array[i + 1] = L -> array[i];
+    }
+    L->array[0] = item; L->size++;
+}
+
+void clear(ArrayListType *l)
+{
+    int i, size = l->size;
+    if(is_empty(l) == 1)
+    {
+        error("삭제할 리스트가 없습니다");
+        return;
+    }
+    /*
+    for( int i = 0 ; i < size - 1 ; i++)
+    {
+        l->array[i] = NULL;    // int 에 NULL 을대입하면 변수에 주소값 0 을 넣는것과 같다고 한다
+    }
+    */
+   //memset(l->array, 0, sizeof(int) * size); 그냥 size 만 0을 만들어도 리스트는 비어있는 상태가 된다 
+    l->size = 0;    // 리스트에 원소가 없으므로 size에 0을 저장
+}
+
+void replace(ArrayListType *l, int pos, element item)
+{
+    if(pos < 0 || pos > l->size - 1)
+    {
+        error("위치 오류");
+        return;
+    }
+    l->array[pos] = item;
+}
+
+int get_length(ArrayListType *l)
+{
+    return l->size;
+}
+
 int main(void)
 {
     ArrayListType list;
 
     init(&list);
-    insert(&list, 0, 10); print_list(&list); // 0번쨰 위치에 10 추가하고 출력
-    insert(&list, 0, 20); print_list(&list); // 0번쨰 위치에 20 추가하고 출력
-    insert(&list, 0, 30); print_list(&list); // 0번쨰 위치에 30 추가하고 출력
+    printf("리스트 0 번쨰 위치에 10 추가 \n");
+    insert_first(&list, 10); print_list(&list); // 0번쨰 위치에 10 추가하고 출력
+    printf("리스트 0 번쨰 위치에 20 추가 \n");
+    insert_first(&list, 20); print_list(&list); // 0번쨰 위치에 20 추가하고 출력
+    printf("리스트 0 번쨰 위치에 30 추가 \n");
+    insert_first(&list, 30); print_list(&list); // 0번쨰 위치에 30 추가하고 출력
+    printf("리스트 맨 끝에 40 추가 \n");
     insert_last(&list, 40); print_list(&list);  // 맨 끝에 40을 추가하고 출력
+    printf("0번째 항목 삭제\n");
     delete(&list, 0);   print_list(&list);  // 0번쨰 항목 삭제하고 출력
+    printf("0번째 원소를 30으로 대체 \n");
+    replace(&list, 0, 30);  print_list(&list);  // 0 번째 원소를 30으로 대체
+    printf("리스트 길이 : %d\n", get_length(&list));
+    printf("리스트 비우기 \n");
+    clear(&list);   print_list(&list);
     return 0;
 }
