553c30f37d590eae76b4b539e1b94fcd930428da
lim971230 <lim971230@gmail.com>
2020-07-19 20:47:58 +0900

    Clear testcase1 and testcase2

--- Algorithms/Algorithms/Programmers_TruckCrossingBridge.cs
+++ Algorithms/Algorithms/Programmers_TruckCrossingBridge.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public static int solution(int bridge_length, int weight, int[] truck_weights) 
---
+++ Algorithms/Programmers_TruckCrossingBridge/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Programmers_TruckCrossingBridge
+{
+    class Program
+    {
+        public static int solution(int bridge_length, int weight, int[] truck_weights)
+        {
+            Queue<int> truckWaiting = new Queue<int>();
+            Queue<int> truckCrossing = new Queue<int>();
+            Queue<int> truckCrossed = new Queue<int>();
+            Dictionary<int, int> timer = new Dictionary<int, int>();
+            int crossingTruckSum = 0;
+            int answer = 0;
+
+            foreach (var truck in truck_weights)
+            {
+                truckWaiting.Enqueue(truck);
+            }
+
+            timer.Add(truckWaiting.Peek(), 1);
+            truckCrossing.Enqueue(truckWaiting.Dequeue());
+            answer++;
+
+            while (true)
+            {
+                if (truckCrossing.Count == 0)
+                {
+                    break;
+                }
+
+                if (timer[truckCrossing.Peek()] == bridge_length)
+                {
+                    truckCrossed.Enqueue(truckCrossing.Dequeue());
+                }
+
+                foreach (var truck in truckCrossing)
+                {
+                    crossingTruckSum += truck;
+                }
+
+                int peekResult;
+                if (truckWaiting.TryPeek(out peekResult))
+                {
+                    if (crossingTruckSum + peekResult <= weight)
+                    {
+                        timer.Add(truckWaiting.Peek(), 0);
+                        truckCrossing.Enqueue(truckWaiting.Dequeue());
+
+                    }
+                    crossingTruckSum = 0;
+                }
+
+                foreach (var truck in timer.ToList())
+                {
+                    timer[truck.Key]++;
+                }
+                answer++;
+            }
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 7, 4, 5, 6};
+            Console.WriteLine(solution(2,10,a));
+            a = new int[] { 10 };
+            Console.WriteLine(solution(100, 100, a));
+            a = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+            Console.WriteLine(solution(100, 100, a));
+        }
+    }
+}
